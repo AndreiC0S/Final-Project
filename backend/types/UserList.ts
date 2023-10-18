a2a66f05ff2b565978f94c1106371154169d1ac4
AndreiC0S <andrei_cos97@yahoo.com>
2023-10-18 14:40:33 +0300

    last commit v1

--- backend/types/UserList.ts
+++
@@ -1,9 +0,0 @@
-export interface UserList {
-    id:number;
-    user_nume: string;
-    user_prenume: string;
-    user_email: string;
-    user_password: string;
-    user_data_nastere: Date;
-    data_adaugare?: Date;
-}
